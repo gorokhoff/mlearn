2071b6875c1ec0d4a462530e3030f7a694f0f497
gorokhov_sa <s.gorokhov@so-line.ru>
2016-02-05 13:39:32 +0300

    добавлено среднее

--- w2_kmeans/kmeans.py
+++ w2_kmeans/kmeans.py
@@ -13,12 +13,24 @@ target = data[['target']]
 
 #1
 from sklearn import cross_validation
+from numpy import mean
 from sklearn.cross_validation import KFold
 kf = KFold(178, n_folds=5, shuffle=True, random_state = 42)
 #2
+lst = [[0,0]]
 from sklearn.neighbors import KNeighborsClassifier
 i = 1
+m1 = 0
+index=0
 while i <= 50:
     neigh = KNeighborsClassifier(n_neighbors=i)
     scores = cross_validation.cross_val_score(neigh, train, target, cv=kf)
+    m = mean(scores)
+    lst.append([i,m])    
+    print(i)
+    if m > m1: 
+        m1 = m 
+        index = i
+    i = i+1
+        
     
